7d6ee46ee27ba70d9f4f9dd0ad95489cf0fef96b
sayjl12 <65030080+sayjl12@users.noreply.github.com>
2020-11-10 22:29:28 +0800

    Deleting some codes and add Level1 class

--- Main.java
+++ Main.java
@@ -27,33 +27,7 @@ public class Main {
 	    
 		background.add(froggerback);
 		
-		background.add(new Log("file:src/p4_group_8_repo/log3.png", 125, 0, 130, 0.75));
-		background.add(new Log("file:src/p4_group_8_repo/log3.png", 125, 220, 130, 0.75));
-		background.add(new Log("file:src/p4_group_8_repo/log3.png", 125, 440, 130, 0.75));
-		background.add(new Log("file:src/p4_group_8_repo/logs.png", 273, 0, 208, -2));
-		background.add(new Log("file:src/p4_group_8_repo/logs.png", 273, 400, 208, -2));
-		background.add(new Log("file:src/p4_group_8_repo/log3.png", 125, 50, 250, 0.75));
-		background.add(new Log("file:src/p4_group_8_repo/log3.png", 125, 270, 250, 0.75));
-		background.add(new Log("file:src/p4_group_8_repo/log3.png", 125, 490, 250, 0.75));
-		
-		background.add(new Turtle(500, 290, -1, 130, 130));
-		background.add(new Turtle(300, 290, -1, 130, 130));
-		background.add(new WetTurtle(700, 290, -1, 130, 130));
-		background.add(new WetTurtle(600, 168, -1, 130, 130));
-		background.add(new WetTurtle(400, 168, -1, 130, 130));
-		background.add(new WetTurtle(200, 168, -1, 130, 130));
-
-		background.add(new Obstacle("file:src/p4_group_8_repo/truck1"+"Right.png", 0, 490, 1, 120, 105));
-		background.add(new Obstacle("file:src/p4_group_8_repo/truck1"+"Right.png", 300, 490, 1, 120, 105));
-		background.add(new Obstacle("file:src/p4_group_8_repo/truck1"+"Right.png", 600, 490, 1, 120, 105));
-		background.add(new Obstacle("file:src/p4_group_8_repo/car1Left.png", 100, 450, -1, 45, 45));
-		background.add(new Obstacle("file:src/p4_group_8_repo/car1Left.png", 250, 450, -1, 45, 45));
-		background.add(new Obstacle("file:src/p4_group_8_repo/car1Left.png", 400, 450, -1, 45, 45));
-		background.add(new Obstacle("file:src/p4_group_8_repo/car1Left.png", 550, 450, -1, 45, 45));
-		background.add(new Obstacle("file:src/p4_group_8_repo/truck2Right.png", 0, 410, 1, 200, 160));
-		background.add(new Obstacle("file:src/p4_group_8_repo/truck2Right.png", 500, 410, 1, 200, 160));
-		background.add(new Obstacle("file:src/p4_group_8_repo/car1Left.png", 500, 370, -5, 45, 45));
-		background.add(new Obstacle("file:src/p4_group_8_repo/car1right.png", 500, 532, 2, 45, 45));
+		Level1 l1 = new Level1(background);
 		
 		background.add(new End(15, 77));
 		background.add(new End(144, 77));
