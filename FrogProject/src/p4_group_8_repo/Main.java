580587b383d028234a5b5952e0794283609c16cb
Jiaxin825 <jx82500@gmail.com>
2020-11-13 22:06:28 +0800

    Testing SourceTree

--- FrogProject/src/p4_group_8_repo/Main.java
+++ FrogProject/src/p4_group_8_repo/Main.java
@@ -23,7 +23,7 @@ public class Main {
 	MySubScene menuSubScene, howToPlaySubScene, HScoreSubScene;
 	List<MyButton> arrayOfButton;
 	Level level1, level2, level;
-	int abc;
+	int abcd;
 
 	public Main(int num) {
 		background = new MyStage();
